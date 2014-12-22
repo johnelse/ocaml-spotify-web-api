fbf7f4280c39639320229d84ed48af55f8bb5f47
John Else <john.else@citrix.com>
2014-12-22 20:53:28 +0000

    Remove unused module open
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/search.ml
+++ lib/search.ml
@@ -1,7 +1,6 @@
 open Lwt
 
 let search mode query parse_fn =
-  let open Cohttp_lwt_unix_io in
   let uri = Uri.of_string
     (Printf.sprintf "%s/search?type=%s&q=%s"
       Common.base_uri (Common.string_of_mode mode) query)
