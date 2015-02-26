fe6d2f50bfa1cf20f0d53475c970475dba146c63
John Else <john.else@citrix.com>
2015-02-26 22:59:38 +0000

    Deduplicate some code by using a polymorphic paging type
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/spotify_web_api.mli
+++ lib/spotify_web_api.mli
@@ -11,7 +11,7 @@ module Common : sig
 end
 
 module Search : sig
-  val search_albums: string -> Album_t.search Lwt.t
-  val search_artists: string -> Artist_t.search Lwt.t
-  val search_tracks: string -> Track_t.search Lwt.t
+  val search_albums: string -> Album_t.album_simplified Paging_t.paging Lwt.t
+  val search_artists: string -> Artist_t.artist Paging_t.paging Lwt.t
+  val search_tracks: string -> Track_t.track Paging_t.paging Lwt.t
 end
